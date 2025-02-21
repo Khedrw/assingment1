cb5f30b12b8788972e101dc9d32d9fa4ff99b418
KDaniel <khderdaniel.77@gmail.com>
2025-02-21 23:18:58 +0200

    6

--- assignment1.py
+++ assignment1.py
@@ -1,11 +1,6 @@
-mounth = input("Enter the full name of the month you storing for : ")
-salary = int(input("Enter your salary for this month : "))
-saving = int(input("Input the percentages for Savings : "))
-savingamount = (saving * salary / 100)
-rent = int(input("Input the cost of rent for this mounth : "))
-electricity = int(input("Input the cost of electricity for this mounth : "))
 
-def initCalculator():
+
+'''def initCalculator():
     TheMonth = input("Enter the name of the month : ")
     TheSalary = int(input("Enter your salary for this month : "))
     new = { 
@@ -13,20 +8,29 @@ def initCalculator():
         "date" : TheMonth,
         "Salary" : TheSalary,
     }
+    print (new)
 
-    return new
+    return new'''
 
 def TheMath():
-    saving = float(input("Enter the percentage of saving: "))
-    rent = int(input("Input the cost of rent for this mounth : "))
-    electricity = int(input("Input the cost of electricity for this mounth : "))
-    x = saving * salary
+    x = saving * salary / 100
     x1 = x + rent + electricity
     if x1 > salary :
         print ("you spend more than your salary")
     else :
-        print (f" The mounth is {mounth} ")
-        print (f" Your salary for this mounth is {salary} $ ")
-        print (f" Percentage of Saving is {saving} % , Which equals {savingamount} $")
-        print (f"The rent will cost you {rent} $ , Which is {round((100*rent/salary),2)} % of your salary")
+        print ("**************************************************")
+        print (f"The mounth is {mounth} Your salary for this mounth is {salary} $\n ")
+        print (f"Percentage of Saving is {saving} % , Which equals {savingamount} $\n")
+        print (f"The rent will cost you {rent} $ , Which is {round((100*rent/salary),2)} % of your salary \n")
         print (f"The electricity will cost you {electricity} $ , Which is {round((100*electricity/salary),2)} % of your salary")
+        print ("**************************************************")
+
+
+mounth = input("Enter the full name of the month you storing for : ")
+salary = int(input("Enter your salary for this month : "))
+saving = int(input("Input the percentages for Savings : "))
+savingamount = (saving * salary / 100)
+rent = int(input("Input the cost of rent for this mounth : "))
+electricity = int(input("Input the cost of electricity for this mounth : "))
+
+TheMath()
